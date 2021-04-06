011b25973c0498209aeea248c44195c9a4f64628
Hopatsa-Oleksandr <bc.alexg@gmail.com>
2021-04-06 20:57:21 +0300

    Добавил pattern

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -47,35 +47,39 @@ class ContactForm extends Component {
 
     return (
       <div className={s.section}>
-      <form className={s.newContacsForm} onSubmit={this.handleSubmit}>
-        <h2 className={s.title}>Добавить новый контакт:</h2>
-        <label className={s.label}>
-          <span className={s.labelTitle}>Имя:</span>
-          <input
-            type="text"
-            onChange={this.changeInput}
-            value={name}
-            name="name"
-            placeholder="введите имя"
-            required
-          />
-        </label>
-        <label className={s.label}>
-          <span className={s.labelTitle}>Номер:</span>
-          <input
-            type="text"
-            onChange={this.changeInput}
-            value={number}
-            name="number"
-            placeholder="введите номер телефона"
-            required
-          />
-        </label>
+        <form className={s.newContacsForm} onSubmit={this.handleSubmit}>
+          <h2 className={s.title}>Добавить новый контакт:</h2>
+          <label className={s.label}>
+            <span className={s.labelTitle}>Имя:</span>
+            <input
+              type="text"
+              onChange={this.changeInput}
+              value={name}
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              placeholder="введите имя"
+              required
+            />
+          </label>
+          <label className={s.label}>
+            <span className={s.labelTitle}>Номер:</span>
+            <input
+              type="text"
+              onChange={this.changeInput}
+              value={number}
+              name="number"
+              pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+              title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+              placeholder="введите номер телефона"
+              required
+            />
+          </label>
 
-        <button type="submit" className={s.button}>
-          Добавить
-        </button>
-      </form>
+          <button type="submit" className={s.button}>
+            Добавить
+          </button>
+        </form>
       </div>
     );
   }
